231428ff595950ef7d8d460a4060e59943b4fded
r888800009 <r888800009@gmail.com>
2020-07-30 20:22:56 +0800

    Add types.pickle

--- lib_dict.py
+++ lib_dict.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import csv
+import pickle
 
 reader = csv.reader(open('libtype.csv'))
 func_dict = {}
@@ -7,3 +8,26 @@ for row in reader:
     func_dict[row[0]] = row[1]
 
 print(func_dict)
+
+types = """Calculation
+File Access
+File and I/O
+File Descriptor Operations
+Memory
+Message Queues
+Network Access
+Process Control
+Shared Memory
+string
+System-Wide
+Time-Related""".split("\n")
+
+type_dict = {}
+
+for i in range(len(types)):
+    type_dict[types[i]] = i
+
+print(type_dict)
+pickle.dump(type_dict, open("types.pickle", "wb"))
+
+
