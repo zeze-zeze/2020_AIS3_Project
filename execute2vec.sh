ec0747aa7e848891108aca0cecddfbdefa0de510
bronson113 <bronson1013@gmail.com>
2020-07-31 14:42:05 +0800

    main train data done

--- execute2vec.sh
+++ execute2vec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python ./objconv/obj2asm.py $1 tmp_file_123123
+python ./obj2asm.py $1 tmp_file_123123
 python3 ./asm_to_vec.py tmp_file_123123 $2
-rm tmp_file_123123
+#rm tmp_file_123123
