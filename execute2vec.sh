e8ce55b7addf946a5f8475da1623fa554c7ec106
bronson113 <bronson1013@gmail.com>
2020-07-31 10:42:53 +0800

    add some documentation

--- execute2vec.sh
+++ execute2vec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python ./objconv/obj2asm.py $1 $2
-python3 ./asm_to_vec.py $2 $3
-rm $2
+python ./objconv/obj2asm.py $1 tmp_file_123123
+python3 ./asm_to_vec.py tmp_file_123123 $2
+rm tmp_file_123123
